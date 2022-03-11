ac8ac7b743de1e281cfdc26dce81eab59de41914
mcdevitt <mcdevo@gmail.com>
2022-03-11 12:01:33 -0500

    - add docstrings to logger functions

--- cli/loggy.py
+++ cli/loggy.py
@@ -5,6 +5,12 @@ from timer import Timer
 
 
 def setup_logger():
+    """
+    initiate logger for main routine
+    all hard coded options at the moment
+    TODO : add filename (log file) and logging level as run-time args or config options
+    :return: logger object
+    """
 
     log_format = "%(levelname)s | %(asctime)s | %(name)s | %(message)s"
 
@@ -16,15 +22,30 @@ def setup_logger():
 
 
 def log_machine(f):
+    """
+    decorator function to add logging capability
+    usage : add @log_machine to functions for which logging is requested
+    logs start time and completion time and time to execute each decorated function
+    assumes : log file and logger previously initiated by setup_logger()
+    :param f: wrapped (decorated) function
+    :return: result of wrapper function
+    """
 
     def wrapper(*args, **kwargs):
+        """
+        executes the logging mechanism at entry and exit of decorated function
+        :param args: args supplied to wrapped function
+        :param kwargs: kwargs supplied to wrapped function
+        :return: returned objs from wrapped function
+        """
 
-        # start the timer, initiate message to log file
+        # start the timer
         t = Timer()
         t.start()
 
+        # start message to log file
         logger = logging.getLogger(__name__)
-        log_message = ('{} | start'.format(f.__name__))
+        log_message = ('%s | start' % f.__name__)
         logger.info(log_message)
 
         # execute the called function
@@ -34,6 +55,7 @@ def log_machine(f):
         log_message = ('%s | complete | %.2f sec' % (f.__name__, t.stop()))
         logger.info(log_message)
 
+        # return control to calling function
         return returned_objs
 
     return wrapper
